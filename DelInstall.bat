bda9cf3f634b76fd27deec945cc12a7062471543
kgulbadam <165226079+kgulbadam@users.noreply.github.com>
2024-04-02 03:53:52 -0400

    Delete DelInstall.bat

--- DelInstall.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-schtasks /delete /s 172.30.227.16 /u s.krivtsov /p 123456 /tn sqs /f
-schtasks /delete /s 172.30.227.16 /u s.krivtsov /p 123456 /tn sql /f
-schtasks /delete /s 172.30.227.16 /u s.krivtsov /p 123456 /tn sqc /f
-del \\172.30.227.16\c$\windows\web\alg.log
-net use /del * /y
