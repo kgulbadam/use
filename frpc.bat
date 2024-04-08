f3bdd1b8aa815d3e75db76c4998b186c3be8d22f
kgulbadam <165226079+kgulbadam@users.noreply.github.com>
2024-04-07 22:34:29 -0400

    Add files via upload

---
+++ frpc.bat
@@ -0,0 +1,8 @@
+@echo off
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+REM
+cd C:\windows\system32\frp
+frpc -c frpc.ini
+exit
