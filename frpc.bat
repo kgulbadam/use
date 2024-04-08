5e1980497c7fd94e4dc0bf20fbe184d651dcb97c
kgulbadam <165226079+kgulbadam@users.noreply.github.com>
2024-04-07 23:03:49 -0400

    Delete frpc.bat

--- frpc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if "%1" == "h" goto begin
-mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
-:begin
-REM
-cd C:\windows\system32\frp
-frpc -c frpc.ini
-exit
